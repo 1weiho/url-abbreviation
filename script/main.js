d2961d298bbe7d4013d9d9ef5069b705f2525475
David Ho <davidho0403@gmail.com>
2022-05-25 14:35:10 +0800

    feat: request backend to search url name

--- script/main.js
+++ script/main.js
@@ -3,3 +3,18 @@ const getUrl = () => {
     return param.get("n");
 };
 
+const searchUrl = (url_name) => {
+    let data = {
+        url_name: url_name,
+    };
+    let json = JSON.stringify(data);
+    $.ajax({
+        url: "backend/core.php",
+        method: "POST",
+        data: json,
+        success: (res) => {
+            res ? jump(res[0]["url_dest"]) : displayError();
+        },
+    });
+};
+
